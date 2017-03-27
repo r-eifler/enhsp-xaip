7e37ab52b885a9d065d7e76a3c1720553876a641
dxli <u5886609@anu.edu.au>
2017-03-27 16:10:24 +1100

    First version of supporters generation completed (not tested).
    1. partition is manually defined;
    2. this implementation makes fo_counters/instance_8 unsolvable. Need to look into the reason.

--- src/heuristics/advanced/Absh.java
+++ src/heuristics/advanced/Absh.java
@@ -5,8 +5,18 @@
  */
 package heuristics.advanced;
 
+import conditions.Comparison;
 import conditions.Conditions;
+import expressions.BinaryOp;
+import expressions.ExtendedNormExpression;
+import expressions.NumEffect;
+import expressions.PDDLNumber;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 import problem.GroundAction;
 
@@ -18,19 +28,88 @@ public class Absh extends Uniform_cost_search_H1_RC{
     
     public Absh(Conditions G, Set<GroundAction> A) {
         super(G, A);
+        this.supporters = new LinkedHashSet();
         
         add_supportes(A);
     }
 
     private void add_supportes(Set<GroundAction> A) {
-        System.out.println("\n\nDEBUG: #Actions, no supporters: "+A.size());
-        this.A.addAll(generate_supporters(A));
-        System.out.println("DEBUG: #Actions, with supporters: " + A.size() +"\n\n");
+        System.out.println("Generating supporters.");
+        generate_supporters();
+        if(this.supporters != null){ this.A.addAll(this.supporters);}
+        System.out.println("Generating supporters finished.");
+        System.out.println("|A U Sup|: " + this.A.size() +"\n\n");
     }
     
-    public LinkedHashSet<GroundAction> generate_supporters(Set<GroundAction> A){
-        LinkedHashSet<GroundAction> actions_plus_action_for_supporters = new LinkedHashSet();
+    private void generate_supporters(){
+        for (GroundAction gr : A) {
+            if (gr.getNumericEffects() != null && !gr.getNumericEffects().sons.isEmpty()) {
+                for (NumEffect effect : (Collection<NumEffect>) gr.getNumericEffects().sons) {
+                    if (!effect.getOperator().equals("assign") || !effect.getRight().rhsFluents().isEmpty()) {
+                      generate_abstract_supporter(effect, gr.toFileCompliant() + effect.getFluentAffected(), gr.getPreconditions(), gr);    
+                    }
+                }
+            }
+        }
+    }
+
+    private void generate_abstract_supporter(NumEffect effect, String name, Conditions preconditions, GroundAction gr) {
+        LinkedHashMap<ArrayList<Float>, Float> precondtions_to_increment;
+        precondtions_to_increment = partition(effect, preconditions, gr);
         
-        return actions_plus_action_for_supporters;
+        for(Map.Entry<ArrayList<Float>, Float> entry : precondtions_to_increment.entrySet()){
+            ArrayList<Float> bounds = entry.getKey();
+            Float lower_bound = bounds.get(0);
+            Float upper_bound = bounds.get(1);
+            Float increment = entry.getValue();
+            
+            GroundAction ret = new GroundAction(name + increment.intValue());
+            
+            // setup effect
+            NumEffect abs_eff = new NumEffect(effect.getOperator());
+            abs_eff.setFluentAffected(effect.getFluentAffected());
+            // new BinaryOp(effect.getRight(), "-", effect.getFluentAffected(), true));
+            abs_eff.setRight(new ExtendedNormExpression(
+                                new BinaryOp(new PDDLNumber(increment),
+                                            effect.getOperator(),
+                                            effect.getFluentAffected(),
+                                            true)
+            ));
+            
+            // setup preconditions. Preconditions = (indirect_preconditions) U pre(gr)
+            Comparison indirect_precondition_gt = new Comparison(">=");
+            Comparison indirect_precondition_lt = new Comparison("<");
+            indirect_precondition_gt.setLeft(effect.getRight());
+            indirect_precondition_gt.setRight(new PDDLNumber(lower_bound));
+            indirect_precondition_lt.setLeft(effect.getRight());
+            indirect_precondition_lt.setRight(new PDDLNumber(upper_bound));
+            
+            // set pre-conditions and effects for the supporter
+            ret.getPreconditions().sons.add(indirect_precondition_lt);
+            ret.getPreconditions().sons.add(indirect_precondition_gt);
+            ret.setPreconditions(ret.getPreconditions().and(preconditions));
+            ret.getNumericEffects().sons.add(abs_eff);
+            
+            // add new supporter
+            this.supporters.add(ret);
+        }
+    }
+
+    private LinkedHashMap<ArrayList<Float>, Float> partition(NumEffect effect, Conditions preconditions, GroundAction gr) {
+        LinkedHashMap<ArrayList<Float>, Float> ret = new LinkedHashMap();
+        
+//        ArrayList<Float> b1 = new ArrayList<>(Arrays.asList(0f,3f));
+//        ArrayList<Float> b2 = new ArrayList<>(Arrays.asList(3f,6f));
+//        ArrayList<Float> b3 = new ArrayList<>(Arrays.asList(6f,10f));
+        
+        
+        ArrayList<Float> b1 = new ArrayList<>(Arrays.asList(0f,5f));
+        ArrayList<Float> b2 = new ArrayList<>(Arrays.asList(5f,10f));
+        
+        ret.put(b1, 1f);
+        ret.put(b2, 5f);
+//        ret.put(b3, 6f);
+        
+        return ret;
     }
 }
