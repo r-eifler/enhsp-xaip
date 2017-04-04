e3da1b7e48756ee1b292fbf8668ac500f4b1557f
dxli <u5886609@anu.edu.au>
2017-04-05 00:51:39 +1000

    Re-implement Absh.
    1. still manual partition;
    2. has a ucs_h1 instance as attribute;
    
    Open issues:
    1. how to set reachable actions initially;
    2. how to automatic partition.

--- src/heuristics/advanced/Absh.java
+++ src/heuristics/advanced/Absh.java
@@ -7,7 +7,6 @@ package heuristics.advanced;
 
 import conditions.Comparison;
 import conditions.Conditions;
-import expressions.BinaryOp;
 import expressions.ExtendedNormExpression;
 import expressions.NumEffect;
 import expressions.PDDLNumber;
@@ -20,40 +19,52 @@ import java.util.Map;
 import java.util.Set;
 import problem.GroundAction;
 import problem.GroundProcess;
+import problem.State;
 
 /**
  *
  * @author dxli
  */
 public class Absh extends ucs_h1_refactored{
+    private ucs_h1_refactored ucs_h1;
     
     public Absh(Conditions G, Set<GroundAction> A, Set<GroundProcess> P) {
         super(G, A, P);
         this.supporters = new LinkedHashSet();
         
-        add_supportes(A);
-    }
-
-    private void add_supportes(Set<GroundAction> A) {
-        System.out.println("Generating supporters.");
         generate_supporters();
-//        if(this.supporters != null){ this.A = (LinkedHashSet <GroundAction>)this.supporters;}
-
-        if(this.supporters != null){ this.A.addAll(this.supporters);}
-        System.out.println("Generating supporters finished.");
-        System.out.println("|A U Sup|: " + this.A.size() +"\n\n");
+        ucs_h1 = new ucs_h1_refactored(G, (Set<GroundAction>) this.supporters, P);
+        ucs_h1.additive_h = true;
+        ucs_h1.red_constraints = true;
+    }
+    
+    @Override
+    public Float setup(State s){
+        // reachability analysis on the abstracted problem
+        Float ret = ucs_h1.setup(s);
+        if (ret == Float.MAX_VALUE){  return ret; }
+        
+        // TODO: How to set up reachable actions initially??
+        this.reachable = A;
+        
+        return ret;
     }
     
     private void generate_supporters(){
+        System.out.println("Generating supporters.");
+        
         for (GroundAction gr : A) {
             if (gr.getNumericEffects() != null && !gr.getNumericEffects().sons.isEmpty()) {
                 for (NumEffect effect : (Collection<NumEffect>) gr.getNumericEffects().sons) {
-                    if (!effect.getOperator().equals("assign") && !effect.getRight().rhsFluents().isEmpty()) {
+                    if (!effect.getOperator().equals("assign") || !effect.getRight().rhsFluents().isEmpty()) {
                       generate_abstract_supporter(effect, gr.toFileCompliant() + effect.getFluentAffected(), gr.getPreconditions(), gr);    
                     }
                 }
             }
         }
+        
+        System.out.println("Generating supporters finished.");
+        System.out.println("|Sup|: " + this.supporters.size());
     }
 
     private void generate_abstract_supporter(NumEffect effect, String name, Conditions preconditions, GroundAction gr) {
@@ -66,18 +77,15 @@ public class Absh extends ucs_h1_refactored{
             Float upper_bound = bounds.get(1);
             Float increment = entry.getValue();
             
-            GroundAction ret = new GroundAction(name + increment.intValue());
+            GroundAction ret = new GroundAction(name + '-' + increment);
             
             // setup effect
             NumEffect abs_eff = new NumEffect(effect.getOperator());
             abs_eff.setFluentAffected(effect.getFluentAffected());
-            // new BinaryOp(effect.getRight(), "-", effect.getFluentAffected(), true));
-            abs_eff.setRight(new ExtendedNormExpression(
-                                new BinaryOp(new PDDLNumber(increment),
-                                            effect.getOperator(),
-                                            effect.getFluentAffected(),
-                                            true)
-            ));
+            abs_eff.setRight(new ExtendedNormExpression(increment));
+            
+            // set effects for supporters
+            ret.getNumericEffects().sons.add(abs_eff);
             
             // setup preconditions. Preconditions = (indirect_preconditions) U pre(gr)
             Comparison indirect_precondition_gt = new Comparison(">=");
@@ -87,34 +95,36 @@ public class Absh extends ucs_h1_refactored{
             indirect_precondition_lt.setLeft(effect.getRight());
             indirect_precondition_lt.setRight(new PDDLNumber(upper_bound));
             
-            // set pre-conditions and effects for the supporter
+            // set pre-conditions for supporters
             ret.getPreconditions().sons.add(indirect_precondition_lt);
             ret.getPreconditions().sons.add(indirect_precondition_gt);
             ret.setPreconditions(ret.getPreconditions().and(preconditions));
-            ret.getNumericEffects().sons.add(abs_eff);
             
             // add new supporter
             this.supporters.add(ret);
-            
-            
+           
+//            System.out.println(ret.toPDDL() + "\n");
         }
     }
 
     private LinkedHashMap<ArrayList<Float>, Float> partition(NumEffect effect, Conditions preconditions, GroundAction gr) {
         LinkedHashMap<ArrayList<Float>, Float> ret = new LinkedHashMap();
         
-//        ArrayList<Float> b1 = new ArrayList<>(Arrays.asList(0f,3f));
-//        ArrayList<Float> b2 = new ArrayList<>(Arrays.asList(3f,6f));
-//        ArrayList<Float> b3 = new ArrayList<>(Arrays.asList(6f,10f));
+        ArrayList<Float> b1 = new ArrayList<>(Arrays.asList(0f,2f));
+        ArrayList<Float> b2 = new ArrayList<>(Arrays.asList(2f,8f));
         
         
-        ArrayList<Float> b1 = new ArrayList<>(Arrays.asList(0f,5f));
-        ArrayList<Float> b2 = new ArrayList<>(Arrays.asList(5f,10f));
+//        ArrayList<Float> b1 = new ArrayList<>(Arrays.asList(0f,2f));
+//        ArrayList<Float> b2 = new ArrayList<>(Arrays.asList(2f,4f));
         
-        ret.put(b1, 2f);
-        ret.put(b2, 6f);
-//        ret.put(b3, 6f);
+        ret.put(b1, 1f);
+        ret.put(b2, 3f);
         
         return ret;
+    }
+    
+    @Override
+    public Float compute_estimate(State s) {
+        return ucs_h1.compute_estimate(s);
     }
 }
