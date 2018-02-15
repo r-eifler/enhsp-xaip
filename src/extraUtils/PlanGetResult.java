dd112e35d31dc769e000dc295694fe49c03a2dda
Enrico Scala <enricos83@gmail.com>
2018-02-15 16:33:02 +0100

    Added first testings

---
+++ src/extraUtils/PlanGetResult.java
@@ -0,0 +1,72 @@
+package extraUtils;
+
+import domain.PddlDomain;
+import heuristics.Aibr;
+import heuristics.blindHeuristic;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import problem.EPddlProblem;
+import search.SearchStrategies;
+
+/*
+ * Copyright (C) 2018 enrico.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+/**
+ *
+ * @author enrico
+ */
+public class PlanGetResult {
+    public int getPlanSize(String domainFileName, String problemFileName, String heuristic) throws Exception{
+        final PddlDomain domain = new PddlDomain(domainFileName);
+        final EPddlProblem problem = new EPddlProblem(problemFileName, domain.getConstants(), domain.getTypes());
+        domain.prettyPrint();
+        domain.validate(problem);
+        
+        final SearchStrategies searchStrategies = new SearchStrategies(); //manager of the search strategies
+        
+        //set deltas in case is a pddl+ problem
+        if (!domain.getProcessesSchema().isEmpty() || !domain.eventsSchema.isEmpty()) {
+            //this is when you have processes
+            problem.setDeltaTimeVariable("1");
+            searchStrategies.delta = Float.parseFloat("1");
+            searchStrategies.processes = true;
+            searchStrategies.delta_max = Float.parseFloat("1");
+        } else {//this is when you have processes
+        }
+        problem.grounding_action_processes_constraints();
+        problem.simplifications_action_processes_constraints();
+
+        
+        //set heuristic
+        if (heuristic.equals("aibr")){
+            searchStrategies.setup_heuristic(new Aibr(problem.getGoals(), problem.getActions(), problem.processesSet, problem.eventsSet));
+            Aibr h = (Aibr) searchStrategies.getHeuristic();
+            h.set(false, true);
+        }else{
+            searchStrategies.setup_heuristic(new blindHeuristic(problem.getGoals(), problem.getActions(), problem.processesSet, problem.eventsSet));
+        }
+        
+        searchStrategies.set_w_g(1);
+        searchStrategies.set_w_h(1);
+        LinkedList raw_plan = searchStrategies.wa_star(problem);
+        //System.out.println(raw_plan.size());
+        return raw_plan.size();
+    }
+}
