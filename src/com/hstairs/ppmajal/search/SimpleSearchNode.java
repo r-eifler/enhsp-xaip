dca7fc07a67f3a897b4977a3c305fc13f5ca5fc7
Enrico <enricos83@gmail.com>
2018-10-03 12:03:26 +0200

    Added simple search node

---
+++ src/com/hstairs/ppmajal/search/SimpleSearchNode.java
@@ -0,0 +1,18 @@
+package com.hstairs.ppmajal.search;
+
+import com.hstairs.ppmajal.problem.State;
+
+public class SimpleSearchNode {
+    final public State s;
+    final public Object transition;
+    final public SimpleSearchNode father;
+    final public float gValue;
+
+
+    public SimpleSearchNode (State s, Object transition, SimpleSearchNode father, float g) {
+        this.s = s;
+        this.transition = transition;
+        this.father = father;
+        this.gValue = g;
+    }
+}
