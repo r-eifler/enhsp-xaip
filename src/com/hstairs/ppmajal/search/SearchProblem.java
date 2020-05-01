559bee146fd0978f5f1fc242af040e8014f12759
Enrico <enricos83@gmail.com>
2020-05-01 21:15:17 +0200

    Adding necessary file

---
+++ src/com/hstairs/ppmajal/search/SearchProblem.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2020 enrico.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package com.hstairs.ppmajal.search;
+
+import com.hstairs.ppmajal.problem.State;
+import java.util.Collection;
+import java.util.Iterator;
+import org.jgrapht.alg.util.Pair;
+
+/**
+ *
+ * @author enrico
+ */
+public abstract class SearchProblem {
+
+    public abstract State getInit();
+
+    public abstract Boolean goalSatisfied(State current);
+
+    public abstract Iterator<Pair<State, Object>> getSuccessors(State s, Collection<Object> actionsToSearch);
+
+    public abstract boolean satisfyGlobalConstraints(State temp);
+
+    public abstract Float gValue(State s, Object act, State temp, float gValue);
+
+    public abstract boolean milestoneReached(Float d, Float current_value, State temp);
+    
+}
