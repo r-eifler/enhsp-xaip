74176c22af91fd5eae1c0b06a02554eeef5b958e
Enrico <enricos83@gmail.com>
2020-05-04 17:07:09 +0200

    Finalised integration with fd instantiator

--- src/com/hstairs/ppmajal/propositionalFactory/FDGrounderInstantiate.java
+++ src/com/hstairs/ppmajal/propositionalFactory/FDGrounderInstantiate.java
@@ -18,18 +18,37 @@
  */
 package com.hstairs.ppmajal.propositionalFactory;
 
-import com.hstairs.ppmajal.domain.ParametersAsTerms;
+import com.hstairs.ppmajal.conditions.AndCond;
+import com.hstairs.ppmajal.conditions.Comparison;
+import com.hstairs.ppmajal.conditions.Condition;
+import com.hstairs.ppmajal.conditions.OrCond;
+import com.hstairs.ppmajal.conditions.Predicate;
+import com.hstairs.ppmajal.domain.PddlDomain;
+import com.hstairs.ppmajal.domain.PredicateSet;
+import com.hstairs.ppmajal.domain.Type;
+import com.hstairs.ppmajal.domain.Variable;
+import com.hstairs.ppmajal.expressions.NumEffect;
+import com.hstairs.ppmajal.expressions.NumFluent;
 import com.hstairs.ppmajal.problem.EPddlProblem;
-import com.hstairs.ppmajal.transition.Transition;
+import com.hstairs.ppmajal.problem.PDDLState;
+import com.hstairs.ppmajal.problem.State;
+import com.hstairs.ppmajal.transition.ConditionalEffects;
 import com.hstairs.ppmajal.transition.TransitionGround;
 import com.hstairs.ppmajal.transition.TransitionSchema;
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jgrapht.alg.util.Pair;
 
 /**
  *
@@ -44,9 +63,9 @@ public class FDGrounderInstantiate extends ExternalGrounder{
     public Collection<TransitionGround> doGrounding(){
         
         final HashMap<String,Collection<List<String>>> groundings = new HashMap();
-        final String sasFileName = "/tmp/temp.sas";
-        String command = "fdtranslator "+domainFile+" "+problemFile;
-        abstractNumeric(domainFile,problemFile);
+        Pair<String, String> dom_prob = abstractNumeric();
+        String command = "fdtranslator "+dom_prob.getFirst()+" "+dom_prob.getSecond();
+        System.out.println(String.format("DEBUG: domain file: %s problem file %s",dom_prob.getFirst(),dom_prob.getSecond()));
         try {
             Process process = Runtime.getRuntime().exec(command);
 
@@ -88,11 +107,225 @@ public class FDGrounderInstantiate extends ExternalGrounder{
                
     }
 
-    private void abstractNumeric(String domainFile, String problemFile) {
+    private Pair<String,String> abstractNumeric() {
+        FileWriter writer = null;
+        String newDF=null,newPF=null;
+        
+        try {
+            String domainInput = "(define (domain temporary)\n "
+                    + ";types \n (:types %s) \n"
+                    + ";predicates \n (:predicates %s)\n"
+                    + ";actions \n %s \n )";
+            StringBuilder actionString  = new StringBuilder();
+            PddlDomain domain = problem.getLinkedDomain();
+            actionString.append(actionString(domain.getActionsSchema()));
+            actionString.append(actionString(domain.eventsSchema));
+            actionString.append(actionString(domain.getProcessesSchema()));
+            StringBuilder predicates = new StringBuilder();
+            predicates.append(buildPredicateString(domain.getPredicates()));
+            predicates.append(buildFunctionsString(domain.functions));
+            StringBuilder types = new StringBuilder();
+            types.append(buildTypes(domain.getTypes()));
+            String toWrite = String.format(domainInput, types, predicates, actionString);
+            File tmpFile = File.createTempFile("domain", ".tmp");
+//            File tmpFile = new File("domaincassic.pddl");
+
+            writer = new FileWriter(tmpFile);
+            writer.write(toWrite);
+            writer.close();
+            newDF = tmpFile.getAbsolutePath();
+        } catch (IOException ex) {
+            Logger.getLogger(FDGrounderInstantiate.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException ex) {
+                Logger.getLogger(FDGrounderInstantiate.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        try {
+            String problemInput = "(define (problem temporary)\n (:domain temporary) "
+                    + ";objects \n  %s \n"
+                    + ";init \n (:init %s)\n"
+                    + ";goals \n (:goal %s) \n )";
+            StringBuilder objects = new StringBuilder();
+            objects.append(problem.objects.pddlPrint());
+            StringBuilder goals = new StringBuilder();
+            goals.append(getPreconditionString(problem.getGoals(),true));
+            StringBuilder init = abstractNumericInit(problem);
+            String toWrite = String.format(problemInput, objects, init,goals);
+            File tmpFile = File.createTempFile("problem", ".tmp");
+//            File tmpFile = new File("problemclassical.pddl");
+
+            writer = new FileWriter(tmpFile);
+            writer.write(toWrite);
+            writer.close();
+            newPF = tmpFile.getAbsolutePath();
+        } catch (IOException ex) {
+            Logger.getLogger(FDGrounderInstantiate.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException ex) {
+                Logger.getLogger(FDGrounderInstantiate.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return Pair.of(newDF, newPF);
+    }
+
+    private StringBuilder actionString(Collection<TransitionSchema> actionsSchema) {
+        String template = "(:action %s \n\t"
+                              + ":parameters (%s) \n\t"
+                              + ":precondition %s \n\t"
+                              + ":effect %s )\n ";
+        StringBuilder str = new StringBuilder();
+        for (TransitionSchema t: actionsSchema){
+            StringBuilder par = new StringBuilder();
+            for (Variable p : (List<Variable>)t.getParameters()){
+                par.append(p.getName()).append(p.getType()).append(" ");
+            }
+            str.append(String.format(template, t.getName(),par,
+                    getPreconditionString(t.getPreconditions(),true),
+                    getEffectString(t.getConditionalPropositionalEffects(),t.getConditionalNumericEffects())));
+        }
+        return str;
+
+    }
+
+    private StringBuilder getPreconditionString(Condition preconditions, boolean toplevel) {
+        if (preconditions instanceof Predicate){
+            return new StringBuilder(preconditions.pddlPrint(false));
+        }
+        if (preconditions instanceof Comparison){
+           String template = "(and %s)";
+           if (!toplevel){
+               template = "%s";
+           }
+           StringBuilder str = new StringBuilder();
+           for (NumFluent nf : preconditions.getInvolvedFluents()){
+               nf.pddlPrint(false,str);
+           }
+            return (new StringBuilder(String.format(template, str.toString())));
+        }
+        if (preconditions instanceof OrCond) {
+            String template = "(or %s)";
+            StringBuilder str = new StringBuilder();
+            for (var v : ((OrCond) preconditions).sons) {
+                str.append(getPreconditionString((Condition) v,false));
+            }
+            return (new StringBuilder(String.format(template, str.toString())));
+        }
+        if (preconditions instanceof AndCond) {
+            String template = "(and %s)";
+            StringBuilder str = new StringBuilder();
+            for (var v : ((AndCond) preconditions).sons) {
+                if (v instanceof NumEffect){
+                    str.append(((NumEffect) v).getFluentAffected().pddlPrint(false));
+                } else {
+                    str.append(getPreconditionString((Condition) v,false));
+                }
+            }
+            return (new StringBuilder(String.format(template, str.toString())));
+        }
+        
+
+        return new StringBuilder(preconditions.pddlPrint(false));
+
+    }
+
+    private StringBuilder getEffectString(ConditionalEffects conditionalPropositionalEffects,
+            ConditionalEffects conditionalNumericEffects) {
+        StringBuilder str = new StringBuilder("");
+        String template0 = "(and %s )";
+
+        StringBuilder effects = new StringBuilder("");
+        effects.append(fromConditionalEffects(conditionalPropositionalEffects.getActualConditionalEffects()));
+        effects.append(fromConditionalEffects(conditionalNumericEffects.getActualConditionalEffects()));
+        effects.append(fromUnconditionalEffects(conditionalPropositionalEffects.getUnconditionalEffect()));
+        effects.append(fromUnconditionalEffects(conditionalNumericEffects.getUnconditionalEffect()));
+        
+        return str.append(String.format(template0, effects));
+    }
+
+    private StringBuilder fromConditionalEffects(Map<Condition, Collection> actualConditionalEffects) {
+        StringBuilder effects = new StringBuilder("");
+        String whenTemplate = "(when %s %s )";
+
+        Set<Map.Entry<Condition, Collection>> entrySet = actualConditionalEffects.entrySet();
+        for (var v : entrySet) {
+            StringBuilder preconditionString = getPreconditionString(v.getKey(),true);
+            StringBuilder effect = new StringBuilder("");
+            for (var v1 : v.getValue()) {
+                basicEffects(v1, effect);
+            }
+            String format = String.format(whenTemplate, preconditionString, effect);
+            effects.append(format);
+        }
+        return effects;
+    }
+
+    private StringBuilder fromUnconditionalEffects(Collection unconditionalEffect) {
+        StringBuilder effects = new StringBuilder("");
+        for (var v : unconditionalEffect) {
+            basicEffects(v,effects);
+        }
+        return effects;
+    }
+ 
+    private void basicEffects(Object v,StringBuilder effects) {
+        if (v instanceof Condition) {
+            effects.append(getPreconditionString((Condition) v,true));
+        } else {
+            if (v instanceof NumEffect) {
+                effects.append(((NumEffect) v).getFluentAffected().pddlPrint(false));
+            }
+        }
+    }
+
+    private StringBuilder buildPredicateString(PredicateSet predicates) {
+        StringBuilder str = new StringBuilder();
+        for (var v: predicates){
+            str.append(v.pddlPrint(true)).append("\n");
+        }
+        return str;
+    }
+    
+
+    private StringBuilder buildFunctionsString(Collection functions) {
+        StringBuilder str = new StringBuilder();
+        for (var v: functions){
+            if (v instanceof NumFluent){
+                str.append(((NumFluent) v).pddlPrint(true)).append("\n");
+            }
+        }
+        return str;    
+    
+    }
+
+    private StringBuilder buildTypes(Set<Type> types) {
+        StringBuilder str = new StringBuilder();
+        for (var v: types){
+            str.append(" ").append(v.getName()).append(" -").append(v.getFatherType());
+        }
+        return str;
+    }
+
+    private StringBuilder abstractNumericInit(EPddlProblem init) {
+        Iterable<NumFluent> numFluentsInvolvedInInit = init.getNumFluentsInvolvedInInit(); 
+        Iterable<Predicate> predicatesInvolvedInInit = init.getPredicatesInvolvedInInit();
+        StringBuilder str =  new StringBuilder("");
+        for (var v : numFluentsInvolvedInInit){
+            
+            str.append(v.pddlPrint(false));
+        }
+        for (var v: predicatesInvolvedInInit){
+            
+            if (!v.getPredicateName().equals("=")){
+                str.append(v.pddlPrint(false));
+            }
+        }
+        return str;
         
     }
 
-    
-
-    
 }
