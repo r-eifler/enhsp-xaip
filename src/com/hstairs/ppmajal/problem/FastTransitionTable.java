fbb9d5b16bd9ae9214a79b5c6b13d97ba2e5ff7f
Enrico Scala <enricos83@gmail.com>
2021-04-25 08:50:33 +0200

    FastTransition table file

---
+++ src/com/hstairs/ppmajal/problem/FastTransitionTable.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hstairs.ppmajal.problem;
+
+import com.hstairs.ppmajal.conditions.Condition;
+import com.hstairs.ppmajal.conditions.PostCondition;
+import java.util.Collection;
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ *
+ * @author enrico
+ */
+public class FastTransitionTable {
+   final public Pair<Condition,PostCondition[]>[][] effects;
+
+    public FastTransitionTable(int nEff) {
+        this.effects = new Pair[nEff][];
+    }
+
+    public void addEffect(int id, Collection<Pair<Condition,Collection<PostCondition>>> eff){
+        this.effects[id] = new Pair[eff.size()];
+        int i = 0;
+        for (final var e : eff){
+            final PostCondition[] effects = new PostCondition[e.getRight().size()];
+            int j = 0;
+            for (final var n : e.getRight()){
+                effects[j] = n;
+                j++;
+            }
+            this.effects[id][i] = Pair.of(e.getKey(),effects);
+            i++;
+        }
+    }
+    
+    public boolean done(int id){
+        return this.effects[id] != null;
+    }
+    
+    public Pair<Condition,PostCondition[]>[] getEffects(int id){
+        return this.effects[id];
+    }
+
+}
