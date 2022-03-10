aafe2dc6cf2829b7d0fc1713381589f6e6e22266
Enrico Scala <enrico@Enricos-MBP.home>
2022-03-10 17:33:12 +0100

    Forgot to add two files!

---
+++ src/com/hstairs/ppmajal/pddl/heuristics/advanced/HGen.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hstairs.ppmajal.pddl.heuristics.advanced;
+
+import com.google.common.collect.Sets;
+import com.hstairs.ppmajal.conditions.Comparison;
+import com.hstairs.ppmajal.conditions.Condition;
+import com.hstairs.ppmajal.conditions.Terminal;
+import com.hstairs.ppmajal.extraUtils.Utils;
+import com.hstairs.ppmajal.pddl.heuristics.advanced.lpsolvers.CPLEXSingleAction;
+import com.hstairs.ppmajal.pddl.heuristics.advanced.lpsolvers.LPSolverSingleAction;
+import com.hstairs.ppmajal.problem.PDDLProblem;
+import com.hstairs.ppmajal.problem.PDDLState;
+import com.hstairs.ppmajal.problem.State;
+import it.unimi.dsi.fastutil.ints.IntArraySet;
+import it.unimi.dsi.fastutil.ints.IntCollection;
+import it.unimi.dsi.fastutil.ints.IntSet;
+import java.util.Arrays;
+import java.util.Set;
+import org.jgrapht.util.FibonacciHeap;
+
+/**
+ *
+ * @author enrico
+ */
+public class HGen extends H1 {
+    
+    final private LPSolverSingleAction[] lps; 
+    private int lpInvokations;
+    private boolean greedy;
+    private final IntArraySet[] activatedActions;
+    public HGen(PDDLProblem problem) {
+        super(problem, false);
+        lps = new CPLEXSingleAction[heuristicNumberOfActions]; 
+        activatedActions = new IntArraySet[heuristicNumberOfActions];
+        greedy = false; //TODO Check the semantics of this
+    }
+
+    @Override
+    public float computeEstimate(State gs) {
+         PDDLState s = (PDDLState) gs;
+        //PriorityQueue<ConditionsNode> q = new PriorityQueue();
+        if (s.satisfy(problem.getGoals())) {
+            return 0f;
+        }
+        
+        FibonacciHeap<Integer> q = new FibonacciHeap();
+        Arrays.fill(getActionHCost(), Float.MAX_VALUE);
+        Arrays.fill(getClosed(), false);
+        for (int v : allActions) {
+            Condition c = preconditionFunction[v];
+            if (c == null || s.satisfy(c)) {
+                actionHCost[v] = 0f;
+                addActionsInPriority(v, q, 0f);
+                closed[v] = true;
+            } else {
+                lps[v] = getLP(v,gs);
+            }
+        }
+
+        IntArraySet activeActions = new IntArraySet();
+        IntArraySet temporaryConditions = new IntArraySet();
+        
+        while (!q.isEmpty()) {
+            float first = -1;
+            while (!q.isEmpty()) {//take all the elements with equal distance from the initial state
+                
+                if (this.additive && !this.reachability) {
+                    if (actionHCost[pseudoGoal] != Float.MAX_VALUE) {
+                        return actionHCost[pseudoGoal] ;
+                    }
+                }       
+                int actionId = q.removeMin().getData();
+
+                if (first == -1) {
+                    first = actionHCost[actionId] ;
+                } else if (first < actionHCost[actionId] ) {//put back and stop
+                    addActionsInPriority(actionId, q, actionHCost[actionId]);
+                    first = -1;
+                    break;//exit from this inner loop and compute cost for new conditons that can be achieved
+                }
+
+                closed[actionId] = true;;//this is the best cost so far; no need to reopen this condition again
+
+                if (actionId == pseudoGoal && !this.reachability) {
+                    if (actionHCost[actionId] == Float.MAX_VALUE) {
+                        System.out.println("Anomaly");
+                    }
+                    return Math.max(actionHCost[actionId], 1f);
+                }
+                activeActions.add(actionId);
+                if (this.reachability) {
+                    this.reachableTransitions.add(actionId);
+                }
+                temporaryConditions.addAll(getActionsAchievableThroughThisAction(actionId));
+
+            }
+            for (int actionId : temporaryConditions) {
+                if (!closed[actionId]) {
+                    Float currentCost = null;
+                    currentCost = lps[actionId].solve(s, activeActions);
+                    lpInvokations++;
+                    if (currentCost != Float.MAX_VALUE) {
+                        if (this.greedy && actionId == pseudoGoal && !this.reachability) {
+                            return Math.max(currentCost, 1f);
+                        }
+                        if (currentCost < getActionHCost()[actionId]) {
+                            if (getActionHCost()[actionId] == Float.MAX_VALUE) {
+                                actionHCost[actionId] = currentCost;
+                                addActionsInPriority(actionId, q, actionHCost[actionId]);
+                            } else {
+                                actionHCost[actionId] = currentCost;
+                                q.decreaseKey(nodeOf[actionId], currentCost);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return Math.max(actionHCost[pseudoGoal], 1f);
+        
+    }
+
+    private IntCollection getActionsAchievableThroughThisAction(int actionId) {
+        final IntArraySet ret = new IntArraySet();
+        IntSet conditionsAchievableById = getInfluencedConditions(actionId);
+        for (int v : allActions) {
+            IntArraySet necessarySet = Utils.getNecessarySet(preconditionFunction[v]);
+            for (int c : conditionsAchievableById) {
+                if (necessarySet.contains(c)) {
+                    ret.add(v);
+                    break;
+                }
+            }
+
+        }
+        return ret;
+    }       
+
+    private IntSet getInfluencedConditions(int actionId) {
+        if (activatedActions[actionId] == null){
+            final IntArraySet achievableTerms = new IntArraySet();
+            for (final int t : getAllComparisons()) {
+                final float v = this.numericContribution(actionId, (Comparison) Terminal.getTerminal(t));
+                if (v > 0 || v == UNKNOWNEFFECT || v < 0) {
+                    achievableTerms.add(t);
+                }
+            }
+            Sets.SetView<Integer> intersection = Sets.intersection(getAllConditions(), (Set<Integer>)propEffectFunction[actionId]);
+            achievableTerms.addAll(intersection);
+            for (final int o : intersection) {
+                updateAchievers(o, actionId);
+            }
+            activatedActions[actionId] = achievableTerms;
+        }
+        
+        return activatedActions[actionId];
+    
+    }
+
+    private LPSolverSingleAction getLP(int actionId, State gs) {
+        if (lps[actionId] == null){
+            final CPLEXSingleAction solver = new CPLEXSingleAction(Utils.getNecessarySet(preconditionFunction[actionId]), allActions, this, actionId);
+            solver.initLp(gs);
+            lps[actionId] = solver;
+        }
+        return lps[actionId];
+    }
+
+
+    public IntArraySet getActionsAchievingAtLeastATerminalOfActionIdPreconditions(int actionId) {
+        final IntArraySet ret = new IntArraySet();
+        final IntArraySet necessarySet = Utils.getNecessarySet(preconditionFunction[actionId]);
+        for (int a: allActions){
+            if (a != actionId){
+                for (int c: necessarySet){
+                    final Terminal t = Terminal.getTerminal(c);
+                    if (t instanceof Comparison comp){
+                        if (this.numericContribution(a, comp) > 0) {
+                            ret.add(a);
+                        }
+                    }else{
+                        if (propEffectFunction[a].contains(c)){
+                            ret.add(a);
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+}
---
+++ src/com/hstairs/ppmajal/pddl/heuristics/advanced/lpsolvers/CPLEXSingleAction.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hstairs.ppmajal.pddl.heuristics.advanced.lpsolvers;
+
+import com.hstairs.ppmajal.conditions.Comparison;
+import com.hstairs.ppmajal.conditions.Terminal;
+import com.hstairs.ppmajal.pddl.heuristics.advanced.HGen;
+import com.hstairs.ppmajal.pddl.heuristics.advanced.LM;
+import com.hstairs.ppmajal.problem.State;
+import com.hstairs.ppmajal.transition.TransitionGround;
+import ilog.concert.IloException;
+import ilog.concert.IloLinearNumExpr;
+import ilog.concert.IloNumVar;
+import ilog.concert.IloNumVarType;
+import ilog.concert.IloRange;
+import ilog.cplex.IloCplex;
+import it.unimi.dsi.fastutil.ints.IntArraySet;
+import java.util.BitSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author enrico
+ */
+public class CPLEXSingleAction extends LPSolverSingleAction {
+    private IloCplex lp;
+    private IloNumVar[] lpvar;
+    private IloRange[] lpcond;
+    private IloLinearNumExpr objectiveFunction;
+    IntArraySet relevantActions;
+    public CPLEXSingleAction(IntArraySet conditions, IntArraySet allActions, HGen h, int actionId) {
+        super(conditions, allActions, h, actionId);
+    }
+
+    
+    @Override
+    public void initLp(State s) {
+        if (lp == null) {
+            try {
+                lp = new IloCplex();
+                relevantActions = h.getActionsAchievingAtLeastATerminalOfActionIdPreconditions(actionId);
+                lpvar = new IloNumVar[h.getHeuristicNumberOfActions()];
+                lpcond = new IloRange[h.getTotNumberOfTerms()];
+                lp.setOut(null);
+                objectiveFunction = lp.linearNumExpr();
+                for (int p : conditions) {
+                    final Terminal terminal = Terminal.getTerminal(p);
+                    final IloLinearNumExpr expr = lp.linearNumExpr();
+                    for (int a : relevantActions) {
+                        float numericContribution = 1f;
+                        if (terminal instanceof Comparison) {
+                            numericContribution = h.getNumericContribution(a, p);
+                        }
+                        if (lpvar[a] == null) {
+                            lpvar[a] = lp.numVar(0.0, 0.0, IloNumVarType.Float);
+                            objectiveFunction.addTerm(lpvar[a], h.getActionCost()[a]);
+                        }
+                        expr.addTerm(lpvar[a], numericContribution);
+                    }
+                    final IloRange ilo = lp.addGe(expr, 0f);
+                    lpcond[p] = ilo;
+                }
+                lp.addMinimize(objectiveFunction);
+            } catch (IloException ex) {
+                Logger.getLogger(LM.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @Override
+    public float solve(State s, IntArraySet activeActions) {
+        try {
+            float preLowerBound = 0f;
+            for (var lm : conditions) {//by default they are not sat in the initial state
+                    final Terminal t = Terminal.getTerminal(lm);
+                    final IloRange constraint = lpcond[lm];
+                    if (t instanceof Comparison) {
+                        double targetValue = -1d * ((Comparison) t).getLeft().eval(s);
+                        constraint.setLB(targetValue);
+                    } else {
+                        if (!s.satisfy(t)){
+                            constraint.setLB(1f);
+                        }
+                    }
+                    for (int a : relevantActions) { //these are reachable actions achieving lm
+                        if (activeActions.contains(a)){
+                            preLowerBound = Math.min(preLowerBound, h.getActionHCost()[a]);
+                            lpvar[a].setUB(Float.MAX_VALUE);
+                        }
+                    }   
+            }
+            if (lp.solve()) {
+                return (float) lp.getObjValue()+preLowerBound;
+            }
+        } catch (IloException ex) {
+            Logger.getLogger(LM.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Float.MAX_VALUE;
+    }
+    
+}
